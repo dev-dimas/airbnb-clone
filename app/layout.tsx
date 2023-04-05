4b6f2e5cc50379c82479cb660bcb0745e01ed01c
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-04-05 12:34:47 +0700

    Add Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google';
 
 import './globals.css';
+import Navbar from '@/components/navbar/Navbar';
 
 export const metadata = {
   title: 'Airbnb',
@@ -14,7 +15,10 @@ const font = Nunito({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
